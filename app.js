d171247c5432cb2e13c55c6ad4d56269489725ae
Ekrem <ekrem@gmail.com>
2023-06-19 16:42:30 +0300

    asyncron logic

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const myPromise = new Promeise((resolve,reject)=>{
-    let condition;
+// const myPromise = new Promeise((resolve,reject)=>{
+//     let condition;
     
-    if(condition is met){
-        resolve("value is logged");
-    }else {
-        reject("value is not logged");
-    }
-})
+//     if(condition is met){
+//         resolve("value is logged");
+//     }else {
+//         reject("value is not logged");
+//     }
+// })
 
-//************************************ */
- //*JS Timing , Event ve http isteklerinde asenkron çalışıyor
+// //************************************ */
+//  //*JS Timing , Event ve http isteklerinde asenkron çalışıyor
+
+console.log("Ekrem");
+setTimeout( ()=>{
+    console.log("sanane")},2000);
+console.log("Yılmaztürk");
