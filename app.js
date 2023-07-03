518ad377c6648d7d91ffc61364d9437e3df6a735
Ekrem <ekrem@gmail.com>
2023-07-03 16:03:19 +0300

    export default

--- app.js
+++ app.js
@@ -82,6 +82,7 @@
 //+  Her COMP içinde bir return barındırıyor. Bu return den sonra JSX kodumuzu yazıp fonk oluşturuyoruz.
 //+  JSX içerisinde JS penceresi açmak istersen {} kullanıyoruz.
 //+  "export const Fonk" diye bir Comp.i export etme yöntemi NAMED export'tur. import ederken import {Fonk} diye çağırmalısın. süslü içine almazsan hata verir.
+//+  "export default" yalnızca bir kere yapılabilir.
 //+  çift süslü ile key-value  yapısı şeklinde inline olarak CSS ile style verilebilir
 //+  yapı içeirisnde yeni bir değişken oluşturarak da istediğim yerde kullanabiliyorum. Content.jsx de parStyle örneği
 //+  Dahili bir resmi eklemek yapmak için eskiden olduğu gibi yol vererek ekleme imkanımız olmuyor. JSX buna izin vermiyor. Resmi import edip süslü içerisinde bir değişken gibi çağpırıyoruz. İSTİSNASI public klasörüdür. public klasörü içerisindeki imajı import etmeden de çağırabiliriz. public kalsörü ana dizin formatında gibi. 
