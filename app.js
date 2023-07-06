557fe072e62f5971146eb2ec8f575b229ad6692a
Ekrem <ekrem@gmail.com>
2023-07-06 22:46:58 +0300

    conditional rendering

--- app.js
+++ app.js
@@ -138,3 +138,4 @@
 //+  EventHandler tanımlarken Component içinde return ün üstünde yazıyoruz.
 //+  Value propu inputlara başlangıç değeri vermemizi sağlar. ve Value'a verilen değer değişken olmalıdır.
 //+  KeyCode a erişmek için Keyboard Event'larını kullanmamız gerekiyor. Yani mesela input'a sadece yazı girilebilsin istiyorum. input type number olsa yazı giremiyorum, text desem ne varsa girilebiliyor. napıoruz Ascii kodlarından keycode değüerine bakarak numaraları devre dışı bırakabiliriz
+//+  CONDITIONAL RENDERING : koşula bağlı olarak bir elementin gösterip- gösterilmemesi durumuna denir.
