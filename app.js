a270e1a79f662f87df1fa74b1a0cd18eb8de6c2e
Ekrem <avekremyilmazturk@gmail.com>
2023-07-11 23:35:44 +0300

    props

--- app.js
+++ app.js
@@ -148,7 +148,7 @@
 //*+  EventHandler tanımlarken Component içinde return ün üstünde yazıyoruz. yani input içerisinde onChange={handleUsername}  tanımladım. yukarı çıkıyorum return üstünde ancak compo. içerisinde klarak ""const handleUsername = () => { } ""tanımını yapıyorum.
 //+  Buradan gelen bilgiyi saklamak ve kullanmak için de napıyorz useState oluşturuyoruz. bunu da kullanmak için en yukarda bu stati import {useState} from "react" diyerek çağırıyoruz.
 //+  OnChange event'ı input degeri her degistiginde tetiklenir. Biz de yazdıgımız event handler araciligi ile State'i guncelleyebilmis oluruz.
-//+  Value propu inputlara başlangıç değeri vermemizi sağlar. ve Value'a verilen değer değişken olmalıdır. statik değer verilirse render durumu ortaya çıkıyor. o yüzden value = {bir state gelir genelde}
+//+  Value propu inputlara başlangıç değeri vermemizi sağlar. ve Value'a verilen değer değişken olmalıdır. statik değer verilirse render durumu ortaya çıkıyor. o yüzden "value = {bir state gelir genelde}"
 //+  KeyCode a erişmek için Keyboard Event'larını kullanmamız gerekiyor. Yani mesela input'a sadece yazı girilebilsin istiyorum. input type number olsa yazı giremiyorum, text desem ne varsa girilebiliyor. napıoruz Ascii kodlarından keycode değüerine bakarak numaraları devre dışı bırakabiliriz
 //+  CONDITIONAL RENDERING : koşula bağlı olarak bir elementin gösterip- gösterilmemesi durumuna denir.
 //?  NBA TW NOTES
