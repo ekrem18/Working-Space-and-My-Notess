27c2244204a893634308660e073291a098e7229c
Ekrem <avekremyilmazturk@gmail.com>
2023-09-02 11:31:02 +0300

    cypress

--- app.js
+++ app.js
@@ -420,3 +420,7 @@
 //+  metadata objesi ve generateMetadata fonksiyonu sadece Server Componentlerden export edilebilir.
 //+  aplikasyonun herhangi bir yerinde gecikme oluştuğunda otomatik loading.js dosyası render edilir
 //+  Error components must be Client Components.aplikasyonun herhangi bir yerinde hata fırlatıldığında otomatik error.js dosyası render edilir
+
+
+//?  *********************************** CYPRESS
+//+  install konuları 11.30'da başlıyo lazımsa
