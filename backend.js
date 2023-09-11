c2232a13521c99747bb4ecd534977669beb309d3
Ekrem <avekremyilmazturk@gmail.com>
2023-09-11 21:38:59 +0300

    new notes v06

--- backend.js
+++ backend.js
@@ -3,4 +3,5 @@
 //+  const içerği değşitirilemese de çalışma mantığı daha hızlıdır.
 //+  {  BURASI BLOCK_SCOPE   }    dolayısıyla burada tanımladığım değişken burada çalışır.
 //+  ` `  backtick kullanılınca dışardan değişken çağırmanın yanında birden fazla satırda işlem yapılabilir.
-//+  
+//+  Array de endeks sabitken , müdahale etmek istersem Object kullanmam gerekiyor key-value ilişkisi.
+//+  JSON şeklinde teknik olarak veri tipi yoktur. Obejct ve array lerin string hali denebilir. 
