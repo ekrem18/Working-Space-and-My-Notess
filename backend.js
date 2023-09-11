6346b3ea130e93669ef31bb9bc56e0666f782d34
Ekrem <avekremyilmazturk@gmail.com>
2023-09-11 21:47:56 +0300

    types

--- backend.js
+++ backend.js
@@ -5,3 +5,6 @@
 //+  ` `  backtick kullanılınca dışardan değişken çağırmanın yanında birden fazla satırda işlem yapılabilir.
 //+  Array de endeks sabitken , müdahale etmek istersem Object kullanmam gerekiyor key-value ilişkisi.
 //+  JSON şeklinde teknik olarak veri tipi yoktur. Obejct ve array lerin string hali denebilir. 
+ /*  let v = '  ' , v bir değişkene sahip ancak verisi yok
+    v = null   da ise v bir değişkene sahipse de değişkenin verisi dahi yok manasındadır.  null False gelir.*/
+//+  
