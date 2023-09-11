8b29b94b148f24b523b97c5feac767f386774958
Ekrem <avekremyilmazturk@gmail.com>
2023-09-11 21:31:46 +0300

    v05

--- backend.js
+++ backend.js
@@ -1,4 +1,6 @@
 //?  *********************************** 11.09
 //+  JS camelCase yazmanı bekler
 //+  const içerği değşitirilemese de çalışma mantığı daha hızlıdır.
+//+  {  BURASI BLOCK_SCOPE   }    dolayısıyla burada tanımladığım değişken burada çalışır.
+//+  ` `  backtick kullanılınca dışardan değişken çağırmanın yanında birden fazla satırda işlem yapılabilir.
 //+  
