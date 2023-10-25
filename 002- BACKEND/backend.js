ae792e88e34666460c8af5095cee3a71032aa854
Ekrem <avekremyilmazturk@gmail.com>
2023-10-25 13:13:42 +0300

    up

--- 002- BACKEND/backend.js
+++ 002- BACKEND/backend.js
@@ -4,12 +4,12 @@
 //+  {  BURASI BLOCK_SCOPE   }    dolayısıyla burada tanımladığım değişken burada çalışır.
 //+  ` `  backtick kullanılınca dışardan değişken çağırmanın yanında birden fazla satırda işlem yapılabilir.
 //+  Array de endeks sabitken , müdahale etmek istersem Object kullanmam gerekiyor key-value ilişkisi.
-//+  JSON şeklinde teknik olarak veri tipi yoktur. Obejct ve array lerin string hali denebilir. 
+//+  JSON şeklinde teknik olarak veri tipi yoktur. Object ve array lerin string hali denebilir. 
  /*  let v = '  ' , v bir değişkene sahip ancak verisi yok
-    v = null   da ise v bir değişkene sahipse de değişkenin verisi dahi yok manasındadır.  null False gelir.*/
+     v = null   da ise v bir değişkene sahipse de değişkenin verisi dahi yok manasındadır.  null False gelir.   */
 //+  (şart)? "x" : "y"     şeklindekli yapı ternary  olarak geçio
 //+  for ve while loops larına dair anlatım ilk ders son saat
-//+  while ve do-while temlede aynı. while da döngüye girmeyebilir şart sağlanmıyorsa , do while da ise mutlaka girilir.
+//+  while ve do-while temelde aynı. while da döngüye girmeyebilir şart sağlanmıyorsa , do while da ise mutlaka girilir.
 //+  arrow func'ta kod tek satırsa süslü ve return yazılması gerekmez.  Global ScopeTur
 //?  *********************************** 13.09
 //+  index.js dosyasında çalışırken projeyi açıp olanları görmek için terminale "node index.js" yazıyoruz
