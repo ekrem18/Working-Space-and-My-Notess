c304d03347f04d0d6e620b559bb81bf0cecac9d9
Ekrem <avekremyilmazturk@gmail.com>
2023-10-25 21:58:26 +0300

    uupp

--- 002- BACKEND/backend.js
+++ 002- BACKEND/backend.js
@@ -594,4 +594,5 @@ const data =await BlogPost.find(search)
 //+  Validate işlemleri sonucu True/False döner
 //*+  Simple Token mantığında; her kullanıcının tek token'ı oluyor. Token olmayan kullanıcı login olamıyor. token silindiğinde oto. logout olmuş oluyor.
 //*+  JWT'ten farklı olarak token'la beraber bilgi taşınmadığı için daha güvenli, de denebilir.
+//+  model hazırlanırken index:true dediğimizde hızlandırma sağlıyoruz
 //+  
