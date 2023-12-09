4847307a2441db46a52217b20851fac045350794
Ekrem <avekremyilmazturk@gmail.com>
2023-12-09 16:09:38 +0300

    up

--- OrgProj/authantication.js
+++ OrgProj/authantication.js
@@ -25,3 +25,43 @@ const crypto = require('node:crypto')
 module.exports = function(password) {
     return crypto.pbkdf25sync(password, keyCode, loopCount, charCount, encType).toString('hex')
 }
+
+
+// auth
+"use strict"
+
+const jwt = require('jsonwebtoken')
+const setToken = require('./setToken')
+
+const user = require ('../issue1/userModel')
+
+module.exports = {
+    login: async (req, res) => {
+        const {email, password} = req.body
+
+        if(email && password){
+            const user = await user.findOne({email, password})
+
+            if(user){
+                if(user.isActive){
+                    res.send({
+                        error: false,
+                        token:setToken(user)
+                    })
+
+                }else{
+                    res.errorStatusCode = 401
+                    throw new Error("User's account is not 'Active'! " )
+                }
+
+            }else{
+                res.errorStatusCode = 401
+                throw new Error (' Wrong email or password')
+            }
+            
+        }else{
+            res.errorStatusCode = 401
+            throw new Error ('Please enter your email and password')
+        }
+    }}
+       
