2036e6ac42551c4145b1b1fff788b16fb8eeb544
Ekrem <avekremyilmazturk@gmail.com>
2023-12-21 16:17:34 +0300

    up

--- 001- FrontEnd/app.js
+++ 001- FrontEnd/app.js
@@ -489,5 +489,5 @@
 //*   Hook Kullanim Kurallari:
 //+  1. İlk olarak import edilmeliler. import { useState } from "react";
 //+  2. Hook'lar ust seviyede kullanilmalidir. Yani Hook'lar bir dongunun, kosul cumleciginin ve icice fonksiyonlarin icerisinde kullanilmamalidir.
-//+  3. Hook'lar sadece React Fonksiyonel componentleri icerisinde cagrilmalidir.Normal Javascript fonksiyonlari icerisinde cagrilmamalidir. (Custom hook'lar icerisinde bir hook cagrilabilir)  
+//+  3. Hook'lar sadece React Fonksiyonel componentleri icerisinde cagrilmalidir.Normal Javascript fonksiyonlari icerisinde cagrilmamalidir. (Custom hook'lar icerisinde bir hook çağırılabilir)  
 //+  
