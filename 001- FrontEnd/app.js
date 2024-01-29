f9fd28fe84e60216b64d1f5b6f1f57fc6608b42e
Ekrem <avekremyilmazturk@gmail.com>
2024-01-29 16:03:26 +0300

    NextJs Workshop

--- 001- FrontEnd/app.js
+++ 001- FrontEnd/app.js
@@ -444,6 +444,9 @@
 //+  metadata objesi ve generateMetadata fonksiyonu sadece Server Componentlerden export edilebilir.
 //+  aplikasyonun herhangi bir yerinde gecikme oluştuğunda otomatik loading.js dosyası render edilir
 //+  Error components must be Client Components.aplikasyonun herhangi bir yerinde hata fırlatıldığında otomatik error.js dosyası render edilir
+//?  *********************************** WorkShop
+//+  React ten farklı olarak Server-Side'da çalışıyor.
+//+  Route yapısı da farklı. Ayrı bir route tanımlaması yapılmasına gerek kalmıyor.
 
 
 //?  *********************************** CYPRESS
